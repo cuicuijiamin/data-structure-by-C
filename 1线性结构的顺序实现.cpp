f5441847e3cb775ef0fe5ade9431647a15bfe558
Phenc <97491430+cuicuijiamin@users.noreply.github.com>
2022-10-20 13:31:43 +0800

    Update 1线性结构的顺序实现.cpp

--- 1线性结构的顺序实现.cpp
+++ 1线性结构的顺序实现.cpp
@@ -1,139 +1,139 @@
-////Âß¼­½á¹¹£ºÏßĞÔ½á¹¹
-////´æ´¢½á¹¹£ºÏßĞÔ´æ´¢
-////Êı¾İÖ®¼äÓĞÏßĞÔ¹ØÏµ
-//#include"M.h"
-//typedef int ElementType;
-//struct sqList
-//{
-//	ElementType* pList;
-//	int length;
-//	int maxSize;
-//}ml;
-//
-//
-//void initList(struct sqList& list);
-//void destroyList(struct sqList& list);
-//void clearList(struct sqList& list);
-//ElementType getElem(struct sqList& list, int index);
-//int findElem(struct sqList& list, ElementType elem);
-//void deleteElem(struct sqList& list, ElementType deleteNum);
-//void alter(struct sqList& list, int index, ElementType alterNum);
-//void expand(struct sqList& list, int size);
-//void insertElem(struct sqList& list, int index, ElementType insertNum);
-//void show(struct sqList& list);
-//
-//void initList(struct sqList &list)
-//{
-//	list.length = 0;
-//	list.maxSize = 0;
-//	list.pList = nullptr;
-//}
-//
-//
-//void destroyList(struct sqList &list)
-//{
-//	delete []list.pList;
-//}
-//
-//void clearList(struct sqList& list)
-//{
-//	list.length = 0;
-//}
-//
-//ElementType getElem(struct sqList& list, int index)
-//{
-//	return list.pList[index];
-//}
-//
-//int findElem(struct sqList& list,ElementType elem)
-//{
-//	//·µ»ØÊ×´Î³öÏÖelemµÄÎ»×Ó---²»´æÔÚÔò·µ»Ø-1
-//	int left = 0;
-//	int right = list.length-1;
-//	while (left < right)
-//	{
-//		int mid = left + (right - left) / 2;
-//		if (list.pList[mid] >= elem)//Ò»¶¨ÔÚmid×ó±ß
-//			right = mid;
-//		else
-//			left = mid + 1;
-//	}
-//	if (list.pList[left] == elem) return left;
-//	else return  -1;
-//}
-//
-//void deleteElem(struct sqList& list, ElementType deleteNum)
-//{
-//	int temp = findElem(list,deleteNum);//ÕÒµ½
-//	for(int i=temp+1;i<list.length;++i)
-//	{
-//		list.pList[i - 1] = list.pList[i];
-//	}
-//	list.length--;
-//}
-//
-//void alter(struct sqList& list,int index, ElementType alterNum)
-//{
-//	list.pList[index] = alterNum;
-//}
-//
-//void expand(struct sqList& list,int size)
-//{
-//	while (list.maxSize < size)
-//	{
-//		list.maxSize += (list.maxSize / 2) > 1 ? (list.maxSize / 2) : 1;
-//	}
-//	//ÈİÁ¿¸Ä±ä¡ª¡ªÖØĞÂ·ÖÅäÄÚ´æ
-//	ElementType* h = new ElementType[list.maxSize];
-//	for (int i = 0; i < list.length; ++i)
-//	{
-//		h[i] = list.pList[i];//·Å½ø´ó¿Õ¼äÖĞ
-//	}
-//	delete []list.pList;
-//	list.pList = h;
-//}
-//
-//void insertElem(struct sqList& list,int index, ElementType insertNum)
-//{
-//	if (list.length +1>list.maxSize)expand(list,list.length+1);
-//	for (int i = list.length-1; i >= index; --i)
-//	{
-//		list.pList[i + 1] = list.pList[i];
-//	}
-//	list.pList[index] = insertNum;
-//	list.length++;
-//}
-//
-//void show(struct sqList& list)
-//{
-//	printf("length=%d\n", list.length);
-//	for (int i = 0; i < list.length; ++i)
-//		printf("pList[%d]=%d\n", i, list.pList[i]);
-//}
-//
-//
-//
-//
-//
-//int main()
-//{
-//	initList(ml);
-//	for (int i = 0; i < 10; ++i)
-//	{
-//		insertElem(ml, i,i * 2);
-//	}
-//	show(ml);
-//	printf("=================================");
-//	alter(ml, 5, 666);
-//	show(ml);
-//	printf("=================================");
-//	deleteElem(ml, 4);
-//	show(ml);
-//	printf("=================================");
-//	int i = findElem(ml, 0);
-//	printf("i=%d\n", i);
-//
-//	clearList(ml);
-//	destroyList(ml);
-//	return 0;
-//}
+//é€»è¾‘ç»“æ„ï¼šçº¿æ€§ç»“æ„
+//å­˜å‚¨ç»“æ„ï¼šçº¿æ€§å­˜å‚¨
+//æ•°æ®ä¹‹é—´æœ‰çº¿æ€§å…³ç³»
+#include"M.h"
+typedef int ElementType;
+struct sqList
+{
+	ElementType* pList;
+	int length;
+	int maxSize;
+}ml;
+
+
+void initList(struct sqList& list);
+void destroyList(struct sqList& list);
+void clearList(struct sqList& list);
+ElementType getElem(struct sqList& list, int index);
+int findElem(struct sqList& list, ElementType elem);
+void deleteElem(struct sqList& list, ElementType deleteNum);
+void alter(struct sqList& list, int index, ElementType alterNum);
+void expand(struct sqList& list, int size);
+void insertElem(struct sqList& list, int index, ElementType insertNum);
+void show(struct sqList& list);
+
+void initList(struct sqList &list)
+{
+	list.length = 0;
+	list.maxSize = 0;
+	list.pList = nullptr;
+}
+
+
+void destroyList(struct sqList &list)
+{
+	delete []list.pList;
+}
+
+void clearList(struct sqList& list)
+{
+	list.length = 0;
+}
+
+ElementType getElem(struct sqList& list, int index)
+{
+	return list.pList[index];
+}
+
+int findElem(struct sqList& list,ElementType elem)
+{
+	//è¿”å›é¦–æ¬¡å‡ºç°elemçš„ä½å­---ä¸å­˜åœ¨åˆ™è¿”å›-1
+	int left = 0;
+	int right = list.length-1;
+	while (left < right)
+	{
+		int mid = left + (right - left) / 2;
+		if (list.pList[mid] >= elem)//ä¸€å®šåœ¨midå·¦è¾¹
+			right = mid;
+		else
+			left = mid + 1;
+	}
+	if (list.pList[left] == elem) return left;
+	else return  -1;
+}
+
+void deleteElem(struct sqList& list, ElementType deleteNum)
+{
+	int temp = findElem(list,deleteNum);//æ‰¾åˆ°
+	for(int i=temp+1;i<list.length;++i)
+	{
+		list.pList[i - 1] = list.pList[i];
+	}
+	list.length--;
+}
+
+void alter(struct sqList& list,int index, ElementType alterNum)
+{
+	list.pList[index] = alterNum;
+}
+
+void expand(struct sqList& list,int size)
+{
+	while (list.maxSize < size)
+	{
+		list.maxSize += (list.maxSize / 2) > 1 ? (list.maxSize / 2) : 1;
+	}
+	//å®¹é‡æ”¹å˜â€”â€”é‡æ–°åˆ†é…å†…å­˜
+	ElementType* h = new ElementType[list.maxSize];
+	for (int i = 0; i < list.length; ++i)
+	{
+		h[i] = list.pList[i];//æ”¾è¿›å¤§ç©ºé—´ä¸­
+	}
+	delete []list.pList;
+	list.pList = h;
+}
+
+void insertElem(struct sqList& list,int index, ElementType insertNum)
+{
+	if (list.length +1>list.maxSize)expand(list,list.length+1);
+	for (int i = list.length-1; i >= index; --i)
+	{
+		list.pList[i + 1] = list.pList[i];
+	}
+	list.pList[index] = insertNum;
+	list.length++;
+}
+
+void show(struct sqList& list)
+{
+	printf("length=%d\n", list.length);
+	for (int i = 0; i < list.length; ++i)
+		printf("pList[%d]=%d\n", i, list.pList[i]);
+}
+
+
+
+
+
+int main()
+{
+	initList(ml);
+	for (int i = 0; i < 10; ++i)
+	{
+		insertElem(ml, i,i * 2);
+	}
+	show(ml);
+	printf("=================================");
+	alter(ml, 5, 666);
+	show(ml);
+	printf("=================================");
+	deleteElem(ml, 4);
+	show(ml);
+	printf("=================================");
+	int i = findElem(ml, 0);
+	printf("i=%d\n", i);
+
+	clearList(ml);
+	destroyList(ml);
+	return 0;
+}
