09db70a6808edc6b02587f763499983cbdab49fe
Phenc <97491430+cuicuijiamin@users.noreply.github.com>
2022-10-20 13:32:53 +0800

    Update 2线性结构的链式实现.cpp

--- 2线性结构的链式实现.cpp
+++ 2线性结构的链式实现.cpp
@@ -1,279 +1,279 @@
-////Âß¼­½á¹¹£ºÏßĞÔ½á¹¹
-////´æ´¢½á¹¹£ºÁ´Ê½´æ´¢¡ª¡ªµ¥Á´±í
-//#include"M.h"
-//typedef struct Data
-//{
-//	int age;
-//	string name;
-//	Data(int x=0,string y="")
-//	{
-//		age = x;
-//		name = y;
-//	}
-//	struct Data& operator =(const struct Data &other)
-//	{
-//		age = other.age;
-//		name = other.name;
-//		return *this;
-//	}
-//	friend ostream& operator<<(ostream& output,const struct Data D)//×¢Òâ£ºÖØÔØÊäÈëÊä³ö±ØĞëÊ¹ÓÃÓÑÔª
-//	{
-//		output << "age=" << D.age << "  name=" << D.name << endl;
-//		return output;
-//	}
-//}ElementType;
-//
-//typedef struct Node
-//{
-//	ElementType data;
-//	struct Node * next;
-//
-//}LNode,*LinkList;
-//
-//void initList(LinkList& L);//
-//void destoryList(LinkList& L);
-//void clearList(LinkList& L);
-//int getListLen(const LinkList& L);//
-//LNode * getListElem(const LinkList& L,int i);//
-//int insertElem(LinkList& L,int i,ElementType insertNum);//
-//int deleteElem(LinkList& L, int i);//
-//int alterElem(LinkList& L, int i,ElementType alterNum);//
-//void show(const LinkList& L);//
-//
-//void initList(LinkList& L)
-//{
-//	LNode* h = new LNode;//¿ª±ÙÍ·½ÚµãÄÚ´æ
-//	h->next = nullptr;//Í·½ÚµãÖ¸ÏòÎª¿Õ
-//	L = h;//L¾ÍÊÇÍ·Ö¸Õë¡ª¡ª´ú±íµÄÊÇÕû¸öÁ´±í
-//}
-//
-//void destoryList(LinkList& L)
-//{
-//   clearList(L);
-//   delete L;//É¾³ıÍ·½Úµã
-//   L = nullptr;//°ÑÍ·½ÚµãÖÃ¿Õ
-//}
-//
-//void clearList(LinkList& L)
-//{
-//	LNode* p = L->next;//Ê×Ôª½Úµã
-//	LNode* q;
-//	while (p)
-//	{
-//		q = p->next;
-//		delete p;
-//		p = q;
-//	}
-//	L->next = nullptr;
-//}
-//
-//int getListLen(const LinkList& L)
-//{
-//	LNode* p = L->next;//Ö¸ÏòµÄÊÇÊ×Ôª½Úµã
-//	int len=0;
-//	while (p)
-//	{
-//		len++;
-//		p = p->next;
-//	}
-//	return len;//³¤¶ÈÊÇ²»°üº¬Í·½ÚµãµÄ
-//}
-//
-//LNode * getListElem(const LinkList& L,int i)
-//{
-//	//Òª»ñÈ¡µÚi¸öÎ»×ÓµÄ½Úµã i=0ÎªÍ·½Úµã
-//	LNode* p = L;//Ö¸ÏòµÄÊÇÍ·½Úµã
-//	int j = 0;
-//	while (p&&j<i)
-//	{
-//		p = p->next;
-//		j++;
-//	}
-//	//Ìø³öÑ­»·£ºÕÒÍêÁË»òÕßÕÒµ½ÁË
-//	if (!p || i < 0)
-//	{
-//		cout << "Ã»ÓĞÕÒµ½iÎ»ÖÃµÄÔªËØ,ÇëÄúÖØĞÂÈ·¶¨Òª²éÕÒµÄÎ»ÖÃ" << endl;
-//	}
-//	return p;
-//}
-//
-//int insertElem(LinkList& L, int i, ElementType insertNum)
-//{
-//	//ÔÚiÇ°Ãæ²åÈëÔªËØ
-//	LNode* p = new LNode;
-//	p->data = insertNum;//Òª²åÈëµÄ½Úµã
-//
-//	LNode* temp = getListElem(L,i-1);
-//
-//	p->next = temp->next;
-//	temp->next = p;
-//	return TRUE;
-//}
-//
-//int alterElem(LinkList& L, int i,ElementType alterNum)
-//{
-//	LNode * p=getListElem(L, i);
-//	p->data = alterNum;
-//	return TRUE;
-//}
-//
-//int deleteElem(LinkList& L, int i)
-//{
-//	LNode* p = getListElem(L, i - 1);
-//	LNode* temp = p->next;
-//	p->next = p->next->next;
-//	delete temp;
-//	return TRUE;
-//}
-//
-//void show(const LinkList& L)
-//{
-//	printf("===========================\n");
-//	LNode* p = L->next;
-//	while (p)
-//	{
-//		cout << p->data;
-//		p = p->next;
-//	}
-//	printf("===========================\n\n");
-//}
-//
-//
-//int main()
-//{
-//	//²âÊÔ
-//	LinkList L;//µ¥Á´±í±íÃû  Í¬Ê±Ò²ÊÇÍ·Ö¸Õë
-//	initList(L);
-//	
-//	insertElem(L, 1, ElementType(10, "sj"));
-//	insertElem(L, 1, ElementType(12, "sakd"));
-//	insertElem(L, 1, ElementType(134, "dcr"));
-//	insertElem(L, 1, ElementType(34, "ewwc"));
-//
-//	show(L);
-//	
-//	deleteElem(L, 2);
-//	show(L);
-//
-//	alterElem(L, 1, ElementType(666,"sawwwwwwwwwww"));
-//	show(L);
-//
-//	printf("%d\n", getListLen(L));
-//	
-//	show(L);
-//
-//	destoryList(L);
-//	return 0;
-//}
+//é€»è¾‘ç»“æ„ï¼šçº¿æ€§ç»“æ„
+//å­˜å‚¨ç»“æ„ï¼šé“¾å¼å­˜å‚¨â€”â€”å•é“¾è¡¨
+#include"M.h"
+typedef struct Data
+{
+	int age;
+	string name;
+	Data(int x=0,string y="")
+	{
+		age = x;
+		name = y;
+	}
+	struct Data& operator =(const struct Data &other)
+	{
+		age = other.age;
+		name = other.name;
+		return *this;
+	}
+	friend ostream& operator<<(ostream& output,const struct Data D)//æ³¨æ„ï¼šé‡è½½è¾“å…¥è¾“å‡ºå¿…é¡»ä½¿ç”¨å‹å…ƒ
+	{
+		output << "age=" << D.age << "  name=" << D.name << endl;
+		return output;
+	}
+}ElementType;
 
-//Á´±íµÄÁ¬½ÓÀıÌâ
-//#include<iostream>
-//using namespace std;
-//typedef struct Node
-//{
-//	int data;
-//	struct Node * next;
-//} LNode, *LinkList;
-//
-//void init(LinkList & L, int len)
-//{
-//	L = new LNode;
-//	L->next = NULL;
-//	LNode * p = L;
-//	int data;
-//
-//	for (int i = 0; i<len; ++i)
-//	{
-//		cin >> data;
-//		LNode * q = new LNode;
-//		q->next = NULL;
-//		q->data = data;
-//		p->next = q;
-//		p = q;
-//	}
-//}
-//
-//void show(LinkList & L)
-//{
-//	LNode * p = L->next;
-//	while (p)
-//	{
-//		printf("%d ", p->data);
-//		p = p->next;
-//	}
-//	printf("\n");
-//}
-//
-//void insertLNode(LinkList &C, LNode * p)//q½ÚµãºóÃæ²åÈëp
-//{
-//	p->next = C->next;
-//	C->next = p;
-//}
-//
-//void link(LinkList & A, LinkList & B, LinkList & C)
-//{
-//	LNode * ta = A->next;//AµÄÊ×Ôª
-//	LNode * tb = B->next;
-//	while (ta && tb)//ÓĞÒ»·½Îª¿ÕÔòÌø³ö
-//	{
-//		if (ta->data<tb->data)
-//		{
-//			LNode * temp = ta->next;
-//			//Í·²åta
-//			insertLNode(C, ta);
-//			ta = temp;
-//		}
-//		else if (tb->data<=ta->data)
-//		{
-//			LNode * temp = tb->next;
-//			insertLNode(C, tb);
-//			tb = temp;
-//		}
-//	}
-//	while (ta)
-//	{
-//		LNode * temp = ta->next;
-//		insertLNode(C, ta);
-//		ta = temp;
-//	}
-//	while (tb)
-//	{
-//		LNode * temp = tb->next;
-//		insertLNode(C, tb);
-//		tb = temp;
-//	}
-//}
-//void destory(LinkList & A, LinkList & B, LinkList & C)
-//{
-//	//É¾³ıA BµÄÍ·½Úµã
-//	delete A;
-//	delete B;
-//
-//	//É¾³ıCÁ´±í
-//	LNode * p = C;
-//	LNode * q;
-//	while (p)
-//	{
-//		q = p->next;
-//		delete p;
-//		p = q;
-//	}
-//}
-//int main()
-//{
-//	int a, b;
-//	cin >> a >> b;
-//	LinkList A;
-//	LinkList B;
-//	init(A, a);
-//	init(B, b);
-//
-//	LinkList C;
-//	init(C, 0);
-//	link(A, B, C);
-//	show(C);
-//
-//	//ÊÍ·Å¿ª±ÙµÄÄÚ´æ
-//	destory(A, B, C);
-//	return 0;
-//}
+typedef struct Node
+{
+	ElementType data;
+	struct Node * next;
+
+}LNode,*LinkList;
+
+void initList(LinkList& L);//
+void destoryList(LinkList& L);
+void clearList(LinkList& L);
+int getListLen(const LinkList& L);//
+LNode * getListElem(const LinkList& L,int i);//
+int insertElem(LinkList& L,int i,ElementType insertNum);//
+int deleteElem(LinkList& L, int i);//
+int alterElem(LinkList& L, int i,ElementType alterNum);//
+void show(const LinkList& L);//
+
+void initList(LinkList& L)
+{
+	LNode* h = new LNode;//å¼€è¾Ÿå¤´èŠ‚ç‚¹å†…å­˜
+	h->next = nullptr;//å¤´èŠ‚ç‚¹æŒ‡å‘ä¸ºç©º
+	L = h;//Lå°±æ˜¯å¤´æŒ‡é’ˆâ€”â€”ä»£è¡¨çš„æ˜¯æ•´ä¸ªé“¾è¡¨
+}
+
+void destoryList(LinkList& L)
+{
+   clearList(L);
+   delete L;//åˆ é™¤å¤´èŠ‚ç‚¹
+   L = nullptr;//æŠŠå¤´èŠ‚ç‚¹ç½®ç©º
+}
+
+void clearList(LinkList& L)
+{
+	LNode* p = L->next;//é¦–å…ƒèŠ‚ç‚¹
+	LNode* q;
+	while (p)
+	{
+		q = p->next;
+		delete p;
+		p = q;
+	}
+	L->next = nullptr;
+}
+
+int getListLen(const LinkList& L)
+{
+	LNode* p = L->next;//æŒ‡å‘çš„æ˜¯é¦–å…ƒèŠ‚ç‚¹
+	int len=0;
+	while (p)
+	{
+		len++;
+		p = p->next;
+	}
+	return len;//é•¿åº¦æ˜¯ä¸åŒ…å«å¤´èŠ‚ç‚¹çš„
+}
+
+LNode * getListElem(const LinkList& L,int i)
+{
+	//è¦è·å–ç¬¬iä¸ªä½å­çš„èŠ‚ç‚¹ i=0ä¸ºå¤´èŠ‚ç‚¹
+	LNode* p = L;//æŒ‡å‘çš„æ˜¯å¤´èŠ‚ç‚¹
+	int j = 0;
+	while (p&&j<i)
+	{
+		p = p->next;
+		j++;
+	}
+	//è·³å‡ºå¾ªç¯ï¼šæ‰¾å®Œäº†æˆ–è€…æ‰¾åˆ°äº†
+	if (!p || i < 0)
+	{
+		cout << "æ²¡æœ‰æ‰¾åˆ°iä½ç½®çš„å…ƒç´ ,è¯·æ‚¨é‡æ–°ç¡®å®šè¦æŸ¥æ‰¾çš„ä½ç½®" << endl;
+	}
+	return p;
+}
+
+int insertElem(LinkList& L, int i, ElementType insertNum)
+{
+	//åœ¨iå‰é¢æ’å…¥å…ƒç´ 
+	LNode* p = new LNode;
+	p->data = insertNum;//è¦æ’å…¥çš„èŠ‚ç‚¹
+
+	LNode* temp = getListElem(L,i-1);
+
+	p->next = temp->next;
+	temp->next = p;
+	return TRUE;
+}
+
+int alterElem(LinkList& L, int i,ElementType alterNum)
+{
+	LNode * p=getListElem(L, i);
+	p->data = alterNum;
+	return TRUE;
+}
+
+int deleteElem(LinkList& L, int i)
+{
+	LNode* p = getListElem(L, i - 1);
+	LNode* temp = p->next;
+	p->next = p->next->next;
+	delete temp;
+	return TRUE;
+}
+
+void show(const LinkList& L)
+{
+	printf("===========================\n");
+	LNode* p = L->next;
+	while (p)
+	{
+		cout << p->data;
+		p = p->next;
+	}
+	printf("===========================\n\n");
+}
+
+
+int main()
+{
+	//æµ‹è¯•
+	LinkList L;//å•é“¾è¡¨è¡¨å  åŒæ—¶ä¹Ÿæ˜¯å¤´æŒ‡é’ˆ
+	initList(L);
+	
+	insertElem(L, 1, ElementType(10, "sj"));
+	insertElem(L, 1, ElementType(12, "sakd"));
+	insertElem(L, 1, ElementType(134, "dcr"));
+	insertElem(L, 1, ElementType(34, "ewwc"));
+
+	show(L);
+	
+	deleteElem(L, 2);
+	show(L);
+
+	alterElem(L, 1, ElementType(666,"sawwwwwwwwwww"));
+	show(L);
+
+	printf("%d\n", getListLen(L));
+	
+	show(L);
+
+	destoryList(L);
+	return 0;
+}
+
+// é“¾è¡¨çš„è¿æ¥ä¾‹é¢˜
+// #include<iostream>
+// using namespace std;
+// typedef struct Node
+// {
+// 	int data;
+// 	struct Node * next;
+// } LNode, *LinkList;
+
+// void init(LinkList & L, int len)
+// {
+// 	L = new LNode;
+// 	L->next = NULL;
+// 	LNode * p = L;
+// 	int data;
+
+// 	for (int i = 0; i<len; ++i)
+// 	{
+// 		cin >> data;
+// 		LNode * q = new LNode;
+// 		q->next = NULL;
+// 		q->data = data;
+// 		p->next = q;
+// 		p = q;
+// 	}
+// }
+
+// void show(LinkList & L)
+// {
+// 	LNode * p = L->next;
+// 	while (p)
+// 	{
+// 		printf("%d ", p->data);
+// 		p = p->next;
+// 	}
+// 	printf("\n");
+// }
+
+// void insertLNode(LinkList &C, LNode * p)//qèŠ‚ç‚¹åé¢æ’å…¥p
+// {
+// 	p->next = C->next;
+// 	C->next = p;
+// }
+
+// void link(LinkList & A, LinkList & B, LinkList & C)
+// {
+// 	LNode * ta = A->next;//Açš„é¦–å…ƒ
+// 	LNode * tb = B->next;
+// 	while (ta && tb)//æœ‰ä¸€æ–¹ä¸ºç©ºåˆ™è·³å‡º
+// 	{
+// 		if (ta->data<tb->data)
+// 		{
+// 			LNode * temp = ta->next;
+// 			//å¤´æ’ta
+// 			insertLNode(C, ta);
+// 			ta = temp;
+// 		}
+// 		else if (tb->data<=ta->data)
+// 		{
+// 			LNode * temp = tb->next;
+// 			insertLNode(C, tb);
+// 			tb = temp;
+// 		}
+// 	}
+// 	while (ta)
+// 	{
+// 		LNode * temp = ta->next;
+// 		insertLNode(C, ta);
+// 		ta = temp;
+// 	}
+// 	while (tb)
+// 	{
+// 		LNode * temp = tb->next;
+// 		insertLNode(C, tb);
+// 		tb = temp;
+// 	}
+// }
+// void destory(LinkList & A, LinkList & B, LinkList & C)
+// {
+// 	//åˆ é™¤A Bçš„å¤´èŠ‚ç‚¹
+// 	delete A;
+// 	delete B;
+
+// 	//åˆ é™¤Cé“¾è¡¨
+// 	LNode * p = C;
+// 	LNode * q;
+// 	while (p)
+// 	{
+// 		q = p->next;
+// 		delete p;
+// 		p = q;
+// 	}
+// }
+// int main()
+// {
+// 	int a, b;
+// 	cin >> a >> b;
+// 	LinkList A;
+// 	LinkList B;
+// 	init(A, a);
+// 	init(B, b);
+
+// 	LinkList C;
+// 	init(C, 0);
+// 	link(A, B, C);
+// 	show(C);
+
+// 	//é‡Šæ”¾å¼€è¾Ÿçš„å†…å­˜
+// 	destory(A, B, C);
+// 	return 0;
+// }
