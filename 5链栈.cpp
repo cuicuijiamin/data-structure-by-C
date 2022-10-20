5d7b58303c0ad47924eabc42068d119c4ac54329
Phenc <97491430+cuicuijiamin@users.noreply.github.com>
2022-10-20 13:34:04 +0800

    Update 5链栈.cpp

--- 5链栈.cpp
+++ 5链栈.cpp
@@ -1,112 +1,112 @@
-////Õ»½á¹¹¡ª¡ªÁ´Õ»
-//#include"M.h"
-//typedef int ElementType;
-//typedef struct Node
-//{
-//	ElementType data;
-//	struct Node * next;//Ö¸ÕëÓò
-//}*StackList,StackNode;
-//
-//void initStack(StackList &S);
-//void destoryStack(StackList &S);
-//void clearStack(StackList & S);
-//bool stackEmpty(StackList &S);
-//void push(StackList &S, ElementType data);
-//void pop(StackList &S);
-//ElementType getTop(StackList & S);
-//void stackTraverse(StackList &S);
-//
-//void initStack(StackList &S)
-//{
-//	S = new StackNode;
-//	S->next = nullptr;//Ö¸ÕëÖ¸Ïò¿Õ¡ª¡ª¿ÕÕ»
-//}
-//void destoryStack(StackList &S)
-//{
-//	StackNode *p = S;//Í·½Úµã
-//	StackNode * q;
-//	while (p)
-//	{
-//		q = p->next;
-//		delete p;
-//		p = q;
-//	}
-//}
-//void clearStack(StackList & S)
-//{
-//
-//	StackNode * p = S->next;
-//	if (p == nullptr)return;
-//	StackNode * q;
-//	while (p)
-//	{
-//		q = p->next;
-//		delete p;
-//		p = q;
-//	}
-//	S->next = nullptr;
-//}
-//bool stackEmpty(StackList &S)
-//{
-//	if (S->next == nullptr)
-//		return true;
-//	return false;
-//}
-//
-//void push(StackList &S,ElementType data)//ÈëÕ»
-//{
-//	StackNode * temp = new StackNode;
-//	temp->data = data;
-//	temp->next = S->next;
-//	S->next = temp;
-//}
-//void pop(StackList &S)//³öÕ»
-//{
-//	if (!stackEmpty(S))
-//	{
-//		StackNode * temp = S->next;
-//		S->next = temp->next;
-//		delete temp;
-//	}
-//	else
-//	{
-//		return;
-//	}
-//}
-//ElementType getTop(StackList & S)
-//{
-//	if (!stackEmpty(S))
-//		return S->next->data;
-//	else
-//		return -100;//ÒâÎªÎÞÐ§Öµ
-//}
-//void stackTraverse(StackList &S)
-//{
-//	printf("===========================\n");
-//	printf("ÉÏ´¦ÎªÕ»¶¥£¬ÏÂ´¦ÎªÕ»µ×\n");
-//	StackNode * temp = S->next;
-//	while (temp)
-//	{
-//		printf("%d\n", temp->data);
-//		temp = temp->next;
-//	}
-//	printf("===========================\n");
-//}
-//
-//int main()
-//{
-//	StackList S;
-//	initStack(S);
-//	printf("%d\n", getTop(S));
-//	for (int i = 0; i < 5; ++i)
-//	{
-//		push(S, i * 10);
-//	}
-//	stackTraverse(S);
-//	printf("%d\n", getTop(S));
-//	pop(S);
-//	stackTraverse(S);
-//	clearStack(S);
-//	destoryStack(S);
-//	return 0;
-//}
+//æ ˆç»“æž„â€”â€”é“¾æ ˆ
+#include"M.h"
+typedef int ElementType;
+typedef struct Node
+{
+	ElementType data;
+	struct Node * next;//æŒ‡é’ˆåŸŸ
+}*StackList,StackNode;
+
+void initStack(StackList &S);
+void destoryStack(StackList &S);
+void clearStack(StackList & S);
+bool stackEmpty(StackList &S);
+void push(StackList &S, ElementType data);
+void pop(StackList &S);
+ElementType getTop(StackList & S);
+void stackTraverse(StackList &S);
+
+void initStack(StackList &S)
+{
+	S = new StackNode;
+	S->next = nullptr;//æŒ‡é’ˆæŒ‡å‘ç©ºâ€”â€”ç©ºæ ˆ
+}
+void destoryStack(StackList &S)
+{
+	StackNode *p = S;//å¤´èŠ‚ç‚¹
+	StackNode * q;
+	while (p)
+	{
+		q = p->next;
+		delete p;
+		p = q;
+	}
+}
+void clearStack(StackList & S)
+{
+
+	StackNode * p = S->next;
+	if (p == nullptr)return;
+	StackNode * q;
+	while (p)
+	{
+		q = p->next;
+		delete p;
+		p = q;
+	}
+	S->next = nullptr;
+}
+bool stackEmpty(StackList &S)
+{
+	if (S->next == nullptr)
+		return true;
+	return false;
+}
+
+void push(StackList &S,ElementType data)//å…¥æ ˆ
+{
+	StackNode * temp = new StackNode;
+	temp->data = data;
+	temp->next = S->next;
+	S->next = temp;
+}
+void pop(StackList &S)//å‡ºæ ˆ
+{
+	if (!stackEmpty(S))
+	{
+		StackNode * temp = S->next;
+		S->next = temp->next;
+		delete temp;
+	}
+	else
+	{
+		return;
+	}
+}
+ElementType getTop(StackList & S)
+{
+	if (!stackEmpty(S))
+		return S->next->data;
+	else
+		return -100;//æ„ä¸ºæ— æ•ˆå€¼
+}
+void stackTraverse(StackList &S)
+{
+	printf("===========================\n");
+	printf("ä¸Šå¤„ä¸ºæ ˆé¡¶ï¼Œä¸‹å¤„ä¸ºæ ˆåº•\n");
+	StackNode * temp = S->next;
+	while (temp)
+	{
+		printf("%d\n", temp->data);
+		temp = temp->next;
+	}
+	printf("===========================\n");
+}
+
+int main()
+{
+	StackList S;
+	initStack(S);
+	printf("%d\n", getTop(S));
+	for (int i = 0; i < 5; ++i)
+	{
+		push(S, i * 10);
+	}
+	stackTraverse(S);
+	printf("%d\n", getTop(S));
+	pop(S);
+	stackTraverse(S);
+	clearStack(S);
+	destoryStack(S);
+	return 0;
+}
