a6452d04c8113fa5b6aaf412fec7d4242517d8f0
Phenc <97491430+cuicuijiamin@users.noreply.github.com>
2022-11-19 19:42:29 +0800

    Update 13二叉排序树.cpp

--- 13二叉排序树.cpp
+++ 13二叉排序树.cpp
@@ -1,48 +1,49 @@
-//#include"M.h"
-//typedef int ElementType;
-//typedef struct BSTNode
-//{
-//	ElementType data;
-//	struct BSTNode * lchild, *rchild;
-//}BSTNode,*BSTree;
-//BSTNode * findBST(BSTNode* T, ElementType num)
-//{
-//	if (T == nullptr || T->data == num)
-//		return T;
-//	else if (T->data < num)
-//		return findBST(T->rchild, num);
-//	else if (T->data>num)
-//		return findBST(T->lchild, num);
-//}
-//void insertBST(BSTree & T, ElementType num)
-//{
-//	if (!T)
-//	{
-//		BSTNode * s = new BSTNode;
-//		s->data = num;
-//		s->lchild = s->rchild = nullptr;
-//		T = s;
-//	}
-//	else if (T->data < num)
-//		insertBST(T->rchild, num);
-//	else if (T->data>num)
-//		insertBST(T->lchild, num);
-//}
-//void deleteBST(BSTree & T, ElementType num)//ɾ��
-//{
-//
-//}
-//void createBSTree(BSTree & T)
-//{
-//	T = nullptr;//��ʼ��
-//	ElementType num;
-//	while (cin >> num)
-//	{
-//		insertBST(T, num);
-//	}
-//}
-//int main()
-//{
-//	BSTree T;
-//	createBSTree(T);
-//}
+//删除功能待实现
+#include"M.h"
+typedef int ElementType;
+typedef struct BSTNode
+{
+	ElementType data;
+	struct BSTNode * lchild, *rchild;
+}BSTNode,*BSTree;
+BSTNode * findBST(BSTNode* T, ElementType num)
+{
+	if (T == nullptr || T->data == num)
+		return T;
+	else if (T->data < num)
+		return findBST(T->rchild, num);
+	else if (T->data>num)
+		return findBST(T->lchild, num);
+}
+void insertBST(BSTree & T, ElementType num)
+{
+	if (!T)
+	{
+		BSTNode * s = new BSTNode;
+		s->data = num;
+		s->lchild = s->rchild = nullptr;
+		T = s;
+	}
+	else if (T->data < num)
+		insertBST(T->rchild, num);
+	else if (T->data>num)
+		insertBST(T->lchild, num);
+}
+void deleteBST(BSTree & T, ElementType num)//É¾³ý
+{
+
+}
+void createBSTree(BSTree & T)
+{
+	T = nullptr;//³õÊ¼»¯
+	ElementType num;
+	while (cin >> num)
+	{
+		insertBST(T, num);
+	}
+}
+int main()
+{
+	BSTree T;
+	createBSTree(T);
+}
